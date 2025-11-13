008faaa09ba92e47c3a90ac756075171d7a011cd
KTykhomyrova <tykhomyrovak@gmail.com>
2025-11-13 10:27:32 +0100

    Add files via upload

---
+++ Mod/Zadanie2/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie2
+{
+    abstract class Zwierze
+    {
+        public string Nazwa { get; protected set; }
+        public int Wiek { get; protected set; }
+
+        public abstract void WydajDzwiek();
+        public abstract void Jedz();
+    }
+
+    class Lew : Zwierze
+    {
+        public override void WydajDzwiek()
+        {
+            Console.WriteLine("Roar!");
+        }
+
+        public override void Jedz()
+        {
+            Console.WriteLine("Ja jem mięso");
+        }
+    }
+
+    class Slon : Zwierze
+    {
+        public override void WydajDzwiek()
+        {
+            Console.WriteLine("Bruu");
+        }
+
+        public override void Jedz()
+        {
+            Console.WriteLine("Ja jem trawę");
+        }
+    }
+
+    class Papuga : Zwierze
+    {
+        public override void WydajDzwiek()
+        {
+            Console.WriteLine("Pi pi!");
+        }
+
+        public override void Jedz()
+        {
+            Console.WriteLine("Ja jem kwiaty");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SymulacjaDnia();
+
+            Console.ReadKey();
+        }
+
+        private static void SymulacjaDnia()
+        {
+            List<Zwierze> zwierzeta = new List<Zwierze>()
+            {
+                new Lew(),
+                new Slon(),
+                new Papuga()
+            };
+
+            Random rand = new Random();
+
+            for (int i = 0; i < zwierzeta.Count; i++)
+            {
+                int los1 = rand.Next(zwierzeta.Count);
+                int los2 = rand.Next(zwierzeta.Count);
+                (zwierzeta[los1], zwierzeta[los2]) = (zwierzeta[los2], zwierzeta[los1]);
+            }
+
+            foreach (var z in zwierzeta)
+            {
+                int metod = rand.Next(2);
+                if (metod == 0)
+                    z.WydajDzwiek();
+                else
+                    z.Jedz();
+            }
+        }
+    }
+}
---
+++ Mod/Zadanie3/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Zadanie3
+{
+    internal class Program
+    {
+        abstract class KontoBankowe
+        {
+            private decimal _stadlo;
+            public decimal Stadlo
+            {
+                get { return _stadlo; }
+                protected set { _stadlo = value; }
+            }
+
+            public KontoBankowe(decimal saldoPoczatkowe = 0)
+            {
+                _stadlo = saldoPoczatkowe;
+            }
+
+
+            public void Wplac(decimal kwota)
+            {
+                if (kwota <= 0)
+                {
+                    Console.WriteLine("Kwota wpłaty musi być dodatnia");
+                    return;
+                }
+                _stadlo += kwota;
+            }
+
+            public virtual void Wyplac(decimal kwota)
+            {
+                if (kwota <= 0)
+                {
+                    Console.WriteLine("Kwota wypłaty musi być dodatnia");
+                    return;
+                }
+                _stadlo -= kwota;
+            }
+        }
+
+        class KontoOszczednosciowe : KontoBankowe
+        {
+            public KontoOszczednosciowe(decimal saldoPoczatkowe = 0) : base(saldoPoczatkowe) { }
+
+            public override void Wyplac(decimal kwota)
+            {
+                if (Stadlo - kwota < 0)
+                {
+                    Console.WriteLine("Nie można wypłacić saldo nie może spaść poniżej zera");
+                    return;
+                }
+                base.Wyplac(kwota);
+            }
+
+            public void ObliczOdsetki()
+            {
+                decimal odsetki = Stadlo * 0.02m;
+                Wplac(odsetki);
+            }
+        }
+
+
+        class KontoPremium : KontoBankowe
+        {
+            public KontoPremium(decimal saldoPoczatkowe = 0) : base(saldoPoczatkowe) { }
+
+            public override void Wyplac(decimal kwota)
+            {
+                if (Stadlo - kwota < -1000)
+                {
+                    Console.WriteLine("Przekroczono dopuszczalny debet");
+                    return;
+                }
+                base.Wyplac(kwota);
+            }
+            static void Main(string[] args)
+            {
+                var konto1 = new KontoPremium(200);
+                var konto2 = new KontoOszczednosciowe(400);
+
+                konto1.Wplac(300);
+                konto1.Wyplac(600);
+                konto1.Wyplac(800);
+
+                konto2.Wplac(100);
+                konto2.Wyplac(200);
+                konto2.Wyplac(400);
+
+                konto2.ObliczOdsetki();
+
+                Console.WriteLine("Stan końcowy kont");
+                Console.WriteLine($"Konto Premium: {konto1.Stadlo} zł");
+                Console.WriteLine($"Konto Oszczędnościowe: {konto2.Stadlo} zł");
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
